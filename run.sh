e8614f21622ee7d1990119d27036c371272060d2
Hastrodis <hastrodis@gmail.com>
2021-05-20 10:50:57 +0700

    deployFIX

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 python ./manage.py migrate
 python ./manage.py collectstatic --noinput
+echo "y" | python manage.py search_index --rebuild
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" | python manage.py shell
 gunicorn --forwarded-allow-ips=* --bind 0.0.0.0:8000 -w 2 InvBase.wsgi:application
